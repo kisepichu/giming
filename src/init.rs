202bca85d78e0bc4bac173ba8d556d62beb7fd61
tqkoh <shktmygs@gmail.com>
2024-04-12 13:30:33 +0900

    impl init

--- src/init.rs
+++ src/init.rs
@@ -1,8 +1,10 @@
 use chrono::{Date, DateTime, TimeZone, Utc};
 use clap::Parser;
+use std::fs;
 use std::process::Output;
 use std::{env, path::Path};
 use std::{io::BufRead, process::Command};
+use tera::Tera;
 
 const F5_INTERVAL_MS: u64 = 100;
 
@@ -17,14 +19,17 @@ pub struct InitArgs {
     overwrite: String,
 }
 
-// url の場合 contest_id に変換する
-fn to_contest_id(s: String) -> String {
-    if s.starts_with("https://atcoder.jp/contests/") {
-        let split = s.split("/").collect::<Vec<&str>>();
+fn change_dir(path: &str) {
+    env::set_current_dir(Path::new(path)).unwrap();
+}
+
+fn to_contest_id(contest_id_or_url: String) -> String {
+    if contest_id_or_url.starts_with("https://atcoder.jp/contests/") {
+        let split = contest_id_or_url.split("/").collect::<Vec<&str>>();
         let contest_id_index = if split.len() > 5 { 5 } else { split.len() - 1 };
         split[contest_id_index].to_string()
     } else {
-        s
+        contest_id_or_url
     }
 }
 
@@ -37,9 +42,9 @@ fn cargo_compete_new(contest_id: &String) -> Output {
         .expect("failed to execute process")
 }
 
-fn create_contest_dir(contest_id: &String) {
+fn create_contest_dir_after_start(contest_id: &String) -> Output {
     let maybe_before = cargo_compete_new(&contest_id);
-    let result = if maybe_before.status.success() {
+    return if maybe_before.status.success() {
         maybe_before
     } else {
         let stderr_last_line = maybe_before.stderr.lines().last().unwrap().unwrap();
@@ -48,12 +53,11 @@ fn create_contest_dir(contest_id: &String) {
             panic!("cargo compete failed: {}", stderr_last_line)
         }
 
-        // 開始時刻まで待つ
+        // 開始時刻 1 秒前まで、 10 秒おき(秒数 1 の位が 0 の時)に現在時刻を表示
         let start_time_str = stderr_last_line
             .split("will begin at ")
             .collect::<Vec<&str>>()[1];
         let start_time = DateTime::parse_from_str(start_time_str, "%Y-%m-%d %H:%M:%S %z").unwrap();
-        // 開始時刻 1 秒前まで、 10 秒おき(秒数 1 の位が 0 の時)に現在時刻を表示
         while start_time.timestamp() - Utc::now().timestamp() > 1 {
             if Utc::now().timestamp() % 10 == 0 {
                 println!("{}", Utc::now().format("%Y-%m-%d %H:%M:%S"));
@@ -70,13 +74,47 @@ fn create_contest_dir(contest_id: &String) {
     };
 }
 
+struct TestCase {
+    input: String,
+    output: String,
+}
+
+struct SolutionContext {
+    contest_id: String,
+    problem_id: String,
+    testcases: Vec<TestCase>,
+}
+
 pub fn init(args: InitArgs) {
     println!("Init: {:?}", args);
 
     let contest_id = to_contest_id(args.contest);
 
-    env::set_current_dir(Path::new("compete")).unwrap();
+    change_dir("compete");
 
-    // コンテストが開始するまで待ち、コンテスト用フォルダを生成する
-    create_contest_dir(&contest_id);
+    create_contest_dir_after_start(&contest_id);
+
+    change_dir(&contest_id);
+
+    let mut paths: Vec<_> = fs::read_dir("src/bin")
+        .unwrap()
+        .map(|x| x.unwrap())
+        .collect();
+    paths.sort_by_key(|x| x.file_name());
+
+    for file in paths {
+        if file.file_type().unwrap().is_file() {
+            let file_name = file.file_name();
+            // .rs を消す
+            let problem_id = file_name
+                .to_str()
+                .unwrap()
+                .to_string()
+                .split('.')
+                .next()
+                .unwrap()
+                .to_string();
+            println!("{}_{}", contest_id, problem_id);
+        }
+    }
 }
