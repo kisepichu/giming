eb7796d263f42d6a90201d6657266d1357a23c7c
tqkoh <shktmygs@gmail.com>
2024-04-15 13:24:49 +0900

    impl save スクレイピング部分も書くように切り替えます

--- compete/abc348/src/bin/b.rs
+++ compete/abc348/src/bin/b.rs
@@ -1,4 +1,24 @@
 use proconio::{input, marker::Usize1 as usize1};
+use std::io::Write;
+
+fn solve(n: usize) {
+    println!("Hello, {}!", n);
+}
+
 fn main() {
-    todo!();
+    input! {
+        n: usize
+    }
+    solve(n);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_solve() {
+        let input = 7;
+        let expected = "Hello, 7!\n";
+    }
 }
