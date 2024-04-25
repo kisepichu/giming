b338bb19cb33a497f78395d9032d3806994768e6
tqkoh <shktmygs@gmail.com>
2024-04-25 14:30:50 +0900

    wip

---
+++ src/usecases/repository_test.rs
@@ -0,0 +1,9 @@
+#[cfg(test)]
+mod repository_test {
+    use super::*;
+    use crate::domain::error::Error;
+    use crate::infrastructure::external::oj_tools_api::LoginServiceResponse;
+
+    #[test]
+    fn test_get_contest() {}
+}
